f0a4c96df38c720b1d56c640c126c7d34c7e4720
Samiullah Khan <kgamers748@gmail.com>
2026-09-03 23:30:34 +0530

    feat: implement user login functionality and enhance registration error handling

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -9,30 +9,59 @@ import { ApiError } from "../utils/apiError.js";
 const registerUser = asyncHandler(async (req, res) => {
     const { fullname, email, password, username } = req.body;
 
-if (!fullname || !email || !password || !username) {
-    throw new ApiError(400, "All fields are required");
-}
-
-const existedUser = await User.findOne(
-    {
-        $or: [{ email }, { username }]
+    if (!fullname || !email || !password || !username) {
+        throw new ApiError(400, "All fields are required");
     }
-)
 
-if (existedUser) {
-    throw new ApiError(409, "User already exists")
-}
+    const existedUser = await User.findOne(
+        {
+            $or: [{ email }, { username }]
+        }
+    )
 
-const newUser = await User.create({
-    fullname,
-    email,
-    password,
-    username: username.toLowerCase()
+    if (existedUser) {
+        throw new ApiError(409, "User already exists")
+    }
+
+    const newUser = await User.create({
+        fullname,
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createUser = await User.findById(newUser.id).select("-password -refreshToken")
+
+    return res.status(201).json(new ApiResponse(200, createUser, "User created successfully"));
 })
 
-const createUser = await User.findById(newUser.id).select("-password -refreshToken")
+const loginUser = asyncHandler(async (req, res) => {
 
-return res.status(201).json(new ApiResponse(200, createUser, "User created successfully"));
+    const { email } = req.body;
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new ApiError(401, "User not found")
+    }
+
+    // await user.isPasswordCorrect(req.body.password)
+
+    // if (isPasswordCorrect != true) {
+    //     throw new ApiError(401, "Invalid password")
+    // }
+
+    const isPasswordValid = await user.isPasswordCorrect(req.body.password)
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid password")
+    }
+
+    user.accessToken = jwt.sign({ id: user.$or({ email }, { username }) }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY })
+
+    user.refreshToken = jwt.sign({ id: user.$or({ email }, { username }) }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRY })
+
+
+    res.send(200).json(new ApiResponse(200, { accessToken: user.accessToken, refreshToken: user.refreshToken }, "User logged in successfully"))
 })
 
-export { registerUser }
+export { registerUser, loginUser }
